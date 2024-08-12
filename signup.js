81bae62040d49a5a9ed8cb1fa8b44951594715eb
Anas <you@example.com>
2024-08-12 14:28:58 +0300

    change language and dark mode & sweetalert

--- signup.js
+++ signup.js
@@ -36,63 +36,71 @@ const inputs = document.querySelectorAll('input');
 // change languages
 const transitions = {
     en:{
-        title: "login",
-        input_email: "Email",
+        title: "sign up",
+        input_name:"User Name",
         input_number: "Phone Number",
+        input_email: "Email",
         input_password: "Password",
-        button:"login",
-        pargr1: "Forgot Password",
-        pargr2: " Don't have an account?",
-        link: "Signup"
+        input_repass:" Re Password",
+        input_Certificate:"Upload Certificate",
+        button:"Sign Up",
+        pargr1: "Have an account? ",
+        link: "login"
     },
     ar:{
-        title: "تسجيل الدخول",
-        input_email: "البريد الالكتروني",
+        title: "التسجيل",
+        input_name:"اسم المستخدم",
         input_number: "رقم الهاتف",
+        input_email: "البريد الالكتروني",
         input_password: "كلمة السر",
-        button:"تسجيل الدخول",
-        pargr1: "نسيت كلمة السر",
-        pargr2: " ليس لديك حساب ؟",
-        link: "تسجيل"
+        input_repass:" تأكيد كلمة السر",
+        input_Certificate:"تسجيل الشهادة",
+        button:"التسجيل",
+        pargr1: " هل لديك حساب ؟",
+        link: "تسجيل الدخول"
     },
     fr:{
-        title: "Connexion",
-        input_email: "Email",
+        title: "Inscription",
+        input_name:"Nom d'utilisateur",
         input_number: "Numéro de téléphone",
+        input_email: "Email",
         input_password: "Mot de passe",
-        button:"Connexion",
-        pargr1: " Mot de passe oublié",
-        pargr2: " Vous n'avez pas de compte ?",
-        link: "Enregistrement"
+        input_repass:" Re mot de passe",
+        input_Certificate:"Télécharger le certificat",
+        button:"Inscription",
+        pargr1: "Avez-vous un compte? ",
+        link: "Connexion"
     },
     deu:{
-        title: "Einloggen",
-        input_email: "Email",
+        title: "Anmelden",
+        input_name:"Benutzername",
         input_number: "Telefonnummer",
+        input_email: "Email",
         input_password: "Passwort",
-        button:"Einloggen",
-        pargr1: "Passwort vergessen",
-        pargr2: " Haben Sie kein Konto ?",
+        input_repass:"Re-Passwort",
+        input_Certificate:"Zertifikat hochladen",
+        button:"Anmelden",
+        pargr1: "Haben Sie ein Konto? ",
         link: "Registrierung"
     }
 }
 
-
 const languageSelectTop = document.querySelector("select");
 let h1 = document.getElementById("h1");
-let lab_email = document.getElementById("lab_email");
+let lab_name = document.getElementById("lab_name");
 let lab_number = document.getElementById("lab_number");
+let lab_email = document.getElementById("lab_email");
 let lab_password = document.getElementById("lab_password");
+let lab_repass = document.getElementById("lab_repass");
+let lab_certifi = document.getElementById("lab_certifi");
 let btn = document.getElementById("btn");
 let p1 = document.getElementById("p1");
-let p2 = document.getElementById("p2");
-let signUp = document.getElementById("signUp");
+let login = document.getElementById("login");
 
-const login = document.getElementById('login');
+const signUp = document.getElementById('signUp');
 
 languageSelectTop.addEventListener("change", (event)=>{
     setLanguage(event.target.value);
-    
 })
 
 const setLanguage = (language)=>{
@@ -112,82 +120,77 @@ const setLanguage = (language)=>{
           Swal.fire("Changes are not saved", "", "info");
         }
       });
-
-    
+    // var confirmMessage = `You are about to change the language to ${language}. Do you want to save this change for all pages?`;
+    // if (confirm(confirmMessage)) {
+       
+    //     localStorage.setItem("language", language);
+    //     alert(`Language changed to ${language}! This change will be applied to all pages.`);
+    //   } else {
+        
+        
+    //     localStorage.removeItem('language');
+    //   }
     if(language == "ar"){
         h1.innerText = transitions.ar.title;
-        lab_email.innerText = transitions.ar. input_email;
+        lab_name.innerText = transitions.ar.input_name;
         lab_number.innerText = transitions.ar. input_number;
+        lab_email.innerText = transitions.ar. input_email;
         lab_password.innerText = transitions.ar.input_password;
+        lab_repass.innerText = transitions.ar.input_repass;
+        lab_certifi.innerText = transitions.ar.input_Certificate;
         btn.innerText = transitions.ar.button;
         p1.innerText = transitions.ar.pargr1;
-        p2.innerText = transitions.ar.pargr2;
-        signUp.innerText = transitions.ar.link;
-        
-        login.style.direction = 'rtl';
-        login.style.textAlign = 'right';
-       
-        
-    //    document.body.style.direction="rtl"
-        // document.getElementById('lab_email').dir = 'rtl';
-        // document.getElementById('lab_number').dir = 'rtl';
-        // document.getElementById('lab_password').dir = 'rtl';
+        login.innerText = transitions.ar.link;
 
-        // document.getElementById('email').dir = 'rtl';
-        // document.getElementById('number').dir = 'rtl';
-        // document.getElementById('password').dir = 'rtl';
-        // document.getElementById('p2').dir = 'rtl';
-        // document.getElementById('signUp').dir = 'rtl';
-        // document.documentElement.dir = 'rtl';
+        signUp.style.direction = 'rtl';
+        signUp.style.textAlign = 'right';
 
     }else if(language == "en"){
-        
         h1.innerText = transitions.en.title;
-        lab_email.innerText = transitions.en.input_email;
-        lab_number.innerText = transitions.en.input_number;
+        lab_name.innerText = transitions.en.input_name;
+        lab_number.innerText = transitions.en. input_number;
+        lab_email.innerText = transitions.en. input_email;
         lab_password.innerText = transitions.en.input_password;
+        lab_repass.innerText = transitions.en.input_repass;
+        lab_certifi.innerText = transitions.en.input_Certificate;
         btn.innerText = transitions.en.button;
         p1.innerText = transitions.en.pargr1;
-        p2.innerText = transitions.en.pargr2;
-        signUp.innerText = transitions.en.link;
+        login.innerText = transitions.en.link;
 
-        login.style.direction = 'ltr';
-        login.style.textAlign = 'left';
+        signUp.style.direction = 'ltr';
+        signUp.style.textAlign = 'left';
     }
     else if(language == "fr"){
-       
         h1.innerText = transitions.fr.title;
-        lab_email.innerText = transitions.fr.input_email;
-        lab_number.innerText = transitions.fr.input_number;
+        lab_name.innerText = transitions.fr.input_name;
+        lab_number.innerText = transitions.fr. input_number;
+        lab_email.innerText = transitions.fr. input_email;
         lab_password.innerText = transitions.fr.input_password;
+        lab_repass.innerText = transitions.fr.input_repass;
+        lab_certifi.innerText = transitions.fr.input_Certificate;
         btn.innerText = transitions.fr.button;
         p1.innerText = transitions.fr.pargr1;
-        p2.innerText = transitions.fr.pargr2;
-        signUp.innerText = transitions.fr.link;
-        
-        login.style.direction = 'ltr';
-        login.style.textAlign = 'left';
+        login.innerText = transitions.fr.link;
+
+        signUp.style.direction = 'ltr';
+        signUp.style.textAlign = 'left';
     }
     else if(language == "deu"){
-        
         h1.innerText = transitions.deu.title;
-        lab_email.innerText = transitions.deu.input_email;
-        lab_number.innerText = transitions.deu.input_number;
+        lab_name.innerText = transitions.deu.input_name;
+        lab_number.innerText = transitions.deu. input_number;
+        lab_email.innerText = transitions.deu. input_email;
         lab_password.innerText = transitions.deu.input_password;
+        lab_repass.innerText = transitions.deu.input_repass;
+        lab_certifi.innerText = transitions.deu.input_Certificate;
         btn.innerText = transitions.deu.button;
         p1.innerText = transitions.deu.pargr1;
-        p2.innerText = transitions.deu.pargr2;
-        signUp.innerText = transitions.deu.link;
+        login.innerText = transitions.deu.link;
 
-        login.style.direction = 'ltr';
-        login.style.textAlign = 'left';
+        signUp.style.direction = 'ltr';
+        signUp.style.textAlign = 'left';
     }
-
-    
 }
-
-
-
 
 
 // dark mode
@@ -210,8 +213,6 @@ const setLanguage = (language)=>{
 //     darkmode = localStorage.getItem('darkmode');
 //     darkmode !=="active" ? enableDarkmode() : disableDarkmode();
 // })
-
-// dark mode
 let darkmode = localStorage.getItem('darkmode');
 const themeswitch = document.getElementById('theme-switch');
 
@@ -223,7 +224,6 @@ const enableDarkmode = ()=>{
         confirmButtonText: "Save",
         // denyButtonText: `Don't save`
       }).then((result) => {
-       
         if (result.isConfirmed) {
             document.body.classList.add("darkmode");
             localStorage.setItem("darkmode", 'active');
@@ -233,8 +233,15 @@ const enableDarkmode = ()=>{
           Swal.fire("Changes are not saved", "", "info");
         }
       });
-
-    
+    // if (confirm("Do you want to switch to dark mode?")) {
+    //     document.body.classList.add("darkmode");
+    //     localStorage.setItem("darkmode", 'active');
+    // }else{
+    //     if (confirm("Do you want to switch to light mode?")) {
+    //         document.body.classList.remove("darkmode");
+            
+    //     }
+    // }
 }
 
 const disableDarkmode = ()=>{
@@ -248,8 +255,4 @@ themeswitch.addEventListener("click", ()=>{
     darkmode = localStorage.getItem('darkmode');
     darkmode !=="active" ? enableDarkmode() : disableDarkmode();
 })
-
-
-
-
 
--- verification.js
+++ verification.js
@@ -14,3 +14,129 @@ scrollright.forEach((el)=>observer.observe(el));
 
 const scrollleft = document.querySelectorAll(".scroll-left");
 scrollleft.forEach((el)=>observer.observe(el));
+
+// change languages
+const transitions = {
+    en:{
+        title: "Inter Code",
+        Text: "The verification code has been sent to your email",
+        pargr1:"*****@gmail.com",
+        pargr2:"The code consists of 6 digits containing letters and numbers and is valid for 10 minutes.",
+        button:"Verify"
+    },
+    ar:{
+        title: "أدخل الرمز",
+        Text: "تم ارسال رمز التحقق الى بريدك الالكتروني",
+        pargr1:"*****@gmail.com",
+        pargr2:"يتكون الرمز من 6 أرقام تشمل أحرفًا وأرقامًا وهو صالح لمدة 10 دقائق",
+        button:"تحقق"
+    },
+    fr:{
+        title: "Code Inter",
+        Text: "Le code de vérification a été envoyé à votre adresse e-mail.",
+        pargr1:"*****@gmail.com",
+        pargr2:"Le code se compose de 6 chiffres contenant des lettres et des chiffres, et il est valable pendant 10 minutes.",
+        button:"Vérifier"
+    },
+    deu:{
+        title: "Inter-Code",
+        Text: "Der Verifizierungscode wurde an Ihre E-Mail-Adresse gesendet.",
+        pargr1:"*****@gmail.com",
+        pargr2:"Der Code besteht aus 6 Ziffern, die Buchstaben und Zahlen enthalten, und ist 10 Minuten lang gültig.",
+        button:"Überprüfen"
+    }
+}
+
+const languageSelectTop = document.querySelector("select");
+let h1 = document.getElementById("h1");
+let h3 = document.getElementById("h3");
+let email = document.getElementById("email");
+let code = document.getElementById("code");
+let btn = document.getElementById("btn");
+
+languageSelectTop.addEventListener("change", (event)=>{
+    setLanguage(event.target.value);
+})
+
+const setLanguage = (language)=>{
+    Swal.fire({
+        title: "Do you want to save the changes?",
+        // showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Save",
+        // denyButtonText: `Don't save`
+      }).then((result) => {
+        
+        if (result.isConfirmed) {
+            localStorage.setItem("language", language);
+          Swal.fire("Saved!", "", "success");
+        } else if (result.isDenied) {
+            localStorage.removeItem('language', language);
+          Swal.fire("Changes are not saved", "", "info");
+        }
+      });
+    if(language == "ar"){
+        h1.innerText = transitions.ar.title;
+        h3.innerText = transitions.ar. Text;
+        email.innerText = transitions.ar. pargr1;
+        code.innerText = transitions.ar.pargr2;
+        btn.innerText = transitions.ar.button;
+
+    }else if(language == "en"){
+        h1.innerText = transitions.en.title;
+        h3.innerText = transitions.en. Text;
+        email.innerText = transitions.en. pargr1;
+        code.innerText = transitions.en.pargr2;
+        btn.innerText = transitions.en.button;
+    }
+    else if(language == "fr"){
+        h1.innerText = transitions.fr.title;
+        h3.innerText = transitions.fr. Text;
+        email.innerText = transitions.fr. pargr1;
+        code.innerText = transitions.fr.pargr2;
+        btn.innerText = transitions.fr.button;
+    }
+    else if(language == "deu"){
+        h1.innerText = transitions.deu.title;
+        h3.innerText = transitions.deu. Text;
+        email.innerText = transitions.deu. pargr1;
+        code.innerText = transitions.deu.pargr2;
+        btn.innerText = transitions.deu.button;
+    }
+}
+
+// dark mode
+let darkmode = localStorage.getItem('darkmode');
+const themeswitch = document.getElementById('theme-switch');
+
+const enableDarkmode = ()=>{
+    Swal.fire({
+        title: "Do you want to save the Dark Mode?",
+        // showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Save",
+        // denyButtonText: `Don't save`
+      }).then((result) => {
+        if (result.isConfirmed) {
+            document.body.classList.add("darkmode");
+            localStorage.setItem("darkmode", 'active');
+          Swal.fire("Saved!", "", "success");
+        } else if (result.isDenied) {
+            document.body.classList.remove("darkmode");
+          Swal.fire("Changes are not saved", "", "info");
+        }
+      });
+}
+
+const disableDarkmode = ()=>{
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmode', null);
+}
+
+if (darkmode === 'active') enableDarkmode();
+
+themeswitch.addEventListener("click", ()=>{
+    darkmode = localStorage.getItem('darkmode');
+    darkmode !=="active" ? enableDarkmode() : disableDarkmode();
+})
+
